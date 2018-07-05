e4ab38c2f4b4d33849e9ac1dae41ad3aff248eb7
Georgi Gardev <georgi.gardev@leanplum.com>
2018-07-05 13:49:14 +0300

    Add command line arguments

--- my_issues.rb
+++ my_issues.rb
@@ -3,13 +3,16 @@ require 'colorize'
 
 require_relative 'jira_config.rb'
 
-def print_my_issues
+def print_my_issues(short, chosen_status)
   client = JIRA::Client.new(JiraConfig::CLIENT_OPTIONS)
   project = JiraConfig::OPTIONS['project']
   my_issues = client.Issue.jql("project=#{project} AND sprint in openSprints() AND assignee='#{JiraConfig::OPTIONS["username"]}'")
 
   my_issues.each do |issue|
     status = issue.status.name
+
+    next if chosen_status && chosen_status != status
+
     status_justified = "(#{status})".rjust(14)
     status_colorized = case status
       when "Done" then status_justified.green
@@ -19,6 +22,10 @@ def print_my_issues
       else status_justified
     end
 
-    print "#{status_colorized} #{issue.key.ljust(7)}: #{issue.summary} (#{JiraConfig::ISSUE_BROWSE_URL}#{issue.key})\n"
+    if short
+      print "#{issue.key.ljust(7)}: (#{JiraConfig::ISSUE_BROWSE_URL}#{issue.key})\n"
+    else
+      print "#{status_colorized} #{issue.key.ljust(7)}: #{issue.summary} (#{JiraConfig::ISSUE_BROWSE_URL}#{issue.key})\n"
+    end
   end
 end

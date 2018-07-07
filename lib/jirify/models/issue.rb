3bec841b2d99e7c99dd7ae4833e1e8c302fae4c8
Georgi Gardev <georgi.gardev@leanplum.com>
2018-07-07 04:38:09 +0300

    Refactoring - use Issue instances

--- lib/jirify/models/issue.rb
+++ lib/jirify/models/issue.rb
@@ -7,34 +7,69 @@ module Jirify
     end
 
     def mine?
-      !@issue.assignee.nil? && @issue.assignee.emailAddress == Config.username
+      !assignee.nil? && assignee.emailAddress == Config.username
     end
 
     def todo?
-      @issue.status.name == Config.statuses['todo']
+      status.name == Config.statuses['todo']
+    end
+
+    def status?(status_name)
+      status.name == status_name
     end
 
     def start!
-      puts "starting #{@issue.summary}..."
+      puts "starting #{summary}..."
       # @issue.save! status: Status.in_progress
     end
 
     def close!
-      puts "closing #{@issue.summary}..."
+      puts "closing #{summary}..."
       # @issue.save! status: Status.closed
+    end
+
+    def print(verbose)
+      status_justified = "(#{status.name})".rjust(14)
+      status_colorized = case status.name
+                         when 'Done'        then status_justified.green
+                         when 'In Progress' then status_justified.blue
+                         when 'In Review'   then status_justified.yellow
+                         when 'Closed'      then status_justified.black
+                         else                    status_justified
+                         end
+
+      url = "#{Config.issue_browse_url}#{key}"
+
+      if verbose
+        puts "#{status_colorized} #{key.ljust(7)}: #{summary} (#{url})"
+      else
+        puts "#{key.ljust(7)}: (#{url})"
+      end
+    end
+
+    def method_missing(method, *args, &_block)
+      if @issue.respond_to? method
+        @issue.send method, *args
+      else
+        super
+      end
+    end
+
+    def respond_to_missing?(method, *)
+      @issue.respond_to? method
     end
 
     class << self
       def list_mine(verbose, statuses = [], all = false)
         my_issues = client.Issue.jql my_issues_jql(all)
+        my_issues.map!     { |issue| Issue.new issue }
         my_issues.sort_by! { |issue| issue.status.name }
 
-        my_issues.each do |issue|
-          status = issue.status.name
-          next if statuses.any? && !statuses.include?(status)
-
-          print_issue(issue, verbose)
+        my_issues.select! do |issue|
+          statuses.empty? || statuses.any? { |status| issue.status? status }
         end
+
+        my_issues.each { |issue| issue.print verbose }
       end
 
       def find_by_id(issue_id)
@@ -46,27 +81,6 @@ module Jirify
       def my_issues_jql(all_issues)
         all_clause = 'AND sprint in openSprints()' unless all_issues
         "project='#{project}' #{all_clause} AND assignee='#{Config.username}'"
-      end
-
-      def print_issue(issue, verbose)
-        status = issue.status.name
-        status_justified = "(#{status})".rjust(14)
-        status_colorized = case status
-                           when 'Done'        then status_justified.green
-                           when 'In Progress' then status_justified.blue
-                           when 'In Review'   then status_justified.yellow
-                           when 'Closed'      then status_justified.black
-                           else                    status_justified
-                           end
-
-        key = issue.key.ljust(7)
-        url = "#{Config.issue_browse_url}#{issue.key}"
-
-        if verbose
-          puts "#{status_colorized} #{key}: #{issue.summary} (#{url})"
-        else
-          puts "#{key}: (#{url})"
-        end
       end
     end
   end

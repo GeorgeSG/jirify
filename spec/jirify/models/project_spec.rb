ce12df513b651d893528e7e80db8b9d0b5bf11df
Georgi Gardev <georgi.gardev@leanplum.com>
2018-07-08 16:07:30 +0300

    Add Project Spec

---
+++ spec/jirify/models/project_spec.rb
@@ -0,0 +1,32 @@
+require 'spec_helper'
+
+describe Jirify::Project do # rubocop:disable RSpec/FilePath
+  before do
+    allow(Jirify::Config).to receive(:options).and_return(mock_config)
+  end
+
+  describe '::all' do
+    let(:project_instance) { instance_double(JIRA::Resource::Project, id: 'key') }
+
+    it 'invokes the Project API' do # rubocop:disable RSpec/ExampleLength
+      call_count = 0
+      allow_any_instance_of(JIRA::Client).to receive_message_chain(:Project, :all) do
+        call_count += 1
+        []
+      end
+
+      described_class.all
+      expect(call_count).to be 1
+    end
+
+    it 'maps returned projects to instances' do
+      allow_any_instance_of(JIRA::Client).to receive_message_chain(:Project, :all) { [project_instance] }
+      expect(described_class.all.first).to be_an(described_class)
+    end
+
+    it 'unwraps values as needed' do
+      allow_any_instance_of(JIRA::Client).to receive_message_chain(:Project, :all) { [project_instance] }
+      expect(described_class.all.first.id).to eq 'key'
+    end
+  end
+end
